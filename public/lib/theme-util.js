fcda74679c6d39f4b506dddbe4bac0465590b0a7
jmquintana <mailjmq@gmail.com>
2024-03-16 20:57:31 -0300

    fixed theme toggle behavior

--- public/lib/theme-util.js
+++ public/lib/theme-util.js
@@ -1,25 +1,31 @@
-console.log(`toggle theme script loaded on ${window.location.pathname}`);
-
 // Get the theme toggle input
-// let themeToggle = document.querySelector(
-//   '.theme-switch input[type="checkbox"]',
-// );
+let themeToggle = document.querySelector(
+  '.theme-switch input[type="checkbox"]',
+);
 
 // Get the current theme from local storage
-// const currentTheme = localStorage.getItem("theme");
+const currentTheme = localStorage.getItem("theme");
+
+// Get the system theme
+const darkModeMediaQuery = window.matchMedia(
+  "(prefers-color-scheme: dark)",
+).matches;
 
 // If the current local storage item can be found
-if (localStorage.getItem("theme")) {
+if (currentTheme) {
   // Set the body data-theme attribute to match the local storage item
-  document.documentElement.setAttribute(
-    "data-theme",
-    localStorage.getItem("theme"),
-  );
+  document.documentElement.setAttribute("data-theme", currentTheme);
 
   // If the current theme is dark, check the theme toggle
   if (localStorage.getItem("theme") === "dark") {
-    document.querySelector('.theme-switch input[type="checkbox"]').checked =
-      true;
+    themeToggle.checked = true;
+  }
+} else {
+  // If the current local storage item can't be found, check the theme toggle if the system theme is dark
+  if (darkModeMediaQuery) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    themeToggle.checked = true;
   }
 }
 
@@ -35,6 +41,4 @@ function switchTheme(e) {
 }
 
 // Add an event listener to the theme toggle, which will switch the theme
-document
-  .querySelector('.theme-switch input[type="checkbox"]')
-  .addEventListener("change", switchTheme, false);
+if (themeToggle) themeToggle.addEventListener("change", switchTheme, false);
