49ac40c18c66302c9fb7ff20b8105cc65edbd3df
josemqu <mailjmq@gmail.com>
2024-07-27 19:26:02 -0300

    full fix

---
+++ src/store.ts
@@ -0,0 +1,24 @@
+import { writable } from "svelte/store";
+
+import { cvES } from "@cv";
+import { cvEN } from "@cv-en";
+
+export const APP_LANGUAGE = {
+  ES: "ES",
+  EN: "EN",
+};
+
+export const appLanguage = writable(APP_LANGUAGE.ES);
+export const appLanguageData = writable({
+  ...(cvES as any),
+});
+
+export const setLanguageToES = () => {
+  appLanguage.set(APP_LANGUAGE.ES);
+  appLanguageData.set(cvES);
+};
+
+export const setLanguageToEN = () => {
+  appLanguage.set(APP_LANGUAGE.EN);
+  appLanguageData.set(cvEN);
+};
