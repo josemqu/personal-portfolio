fd39c5ed1c151403ccfc43e335144b9e8bc7424b
josemqu <mailjmq@gmail.com>
2024-07-27 20:04:54 -0300

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   output: "hybrid",
+  adapter: vercel()
 });
