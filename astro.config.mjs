6a483ac8ab6b4c233df3282439479889f8e43abf
josemqu <mailjmq@gmail.com>
2024-07-27 19:53:16 -0300

    merge fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-<<<<<<< HEAD
   output: "hybrid",
-=======
-  output: "server",
->>>>>>> 40e6ebf70c6bedc83c245672043cb40dab10480c
 });
