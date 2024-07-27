b521234b780710fba81904e99265a1d00f3fbeee
josemqu <mailjmq@gmail.com>
2024-07-27 19:42:08 -0300

    store commented

--- src/store.ts
+++ src/store.ts
@@ -1,24 +1,24 @@
 import { writable } from "svelte/store";
 
-import { cvES } from "@cv";
-import { cvEN } from "@cv-en";
+// import { cvES } from "@cv";
+// import { cvEN } from "@cv-en";
 
-export const APP_LANGUAGE = {
-  ES: "ES",
-  EN: "EN",
-};
+// export const APP_LANGUAGE = {
+//   ES: "ES",
+//   EN: "EN",
+// };
 
-export const appLanguage = writable(APP_LANGUAGE.ES);
-export const appLanguageData = writable({
-  ...(cvES as any),
-});
+// export const appLanguage = writable(APP_LANGUAGE.ES);
+// export const appLanguageData = writable({
+//   ...(cvES as any),
+// });
 
-export const setLanguageToES = () => {
-  appLanguage.set(APP_LANGUAGE.ES);
-  appLanguageData.set(cvES);
-};
+// export const setLanguageToES = () => {
+//   appLanguage.set(APP_LANGUAGE.ES);
+//   appLanguageData.set(cvES);
+// };
 
-export const setLanguageToEN = () => {
-  appLanguage.set(APP_LANGUAGE.EN);
-  appLanguageData.set(cvEN);
-};
+// export const setLanguageToEN = () => {
+//   appLanguage.set(APP_LANGUAGE.EN);
+//   appLanguageData.set(cvEN);
+// };
