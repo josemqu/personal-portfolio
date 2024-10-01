3a29a37d80670bdefa1924c00b6ac8f7f7d4416b
josemqu <mailjmq@gmail.com>
2024-09-30 22:16:48 -0300

    added audio to the hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   output: "hybrid",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [react()]
 });
---
+++ src/components/Player.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from "react";
+
+const Play = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="currentColor"
+  >
+    <path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"></path>
+  </svg>
+);
+
+const Pause = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="currentColor"
+  >
+    <path d="M520-200v-560h240v560H520Zm-320 0v-560h240v560H200Zm400-80h80v-400h-80v400Zm-320 0h80v-400h-80v400Zm0-400v400-400Zm320 0v400-400Z" />
+  </svg>
+);
+
+export function Player() {
+  const [playing, setPlaying] = useState(false);
+  const audioRef = useRef();
+
+  const handleClick = () => {
+    if (playing) {
+      console.log("pause");
+      audioRef.current.pause();
+      audioRef.current.volume = 0.5;
+    } else {
+      console.log("play");
+      audioRef.current.play();
+      audioRef.current.volume = 0.5;
+      audioRef.current.src = `/sounds/sound.mp3`;
+    }
+
+    setPlaying(!playing);
+  };
+
+  return (
+    <div>
+      <div onClick={handleClick}>{playing ? <Pause /> : <Play />}</div>
+      <audio ref={audioRef} src={`/sounds/sound.mp3`} />
+    </div>
+  );
+}
